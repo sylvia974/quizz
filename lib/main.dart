9f900be966fd26c4361c383140e39d16641db99f
paquiry <paquirysylvia974@gmail.com>
2022-01-18 12:52:39 +0400

    3 - Les listes

--- lib/main.dart
+++ lib/main.dart
@@ -6,87 +6,99 @@ void main() {
 
 class quizz extends StatefulWidget {
   const quizz({Key? key}) : super(key: key);
+
   @override
   _quizzState createState() => _quizzState();
 }
 
 class _quizzState extends State<quizz> {
-  List<Icon> score =[
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
 
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-  ];
+  List<Icon> suiviscore = [
+  Icon(Icons.check,color:Colors.green),
+  Icon(Icons.check,color:Colors.green),
+  Icon(Icons.close,color:Colors.red),
+  Icon(Icons.check,color:Colors.green),
+  Icon(Icons.check,color:Colors.green),
+];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  flex: 5,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Center(
-                      child: Text(
-                        'Voici le texte de la question',
-                        style: TextStyle(
-                          color: Colors.white,
-                          letterSpacing: 2.5,
-                          fontSize: 25.0,
-                        ),
-                      ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+
+              Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:10.0),
+                  child: Center(
+                    child: Text("Ici sera affiché les questions",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25.0,
+                    ),
                     ),
                   ),
                 ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: TextButton(
-                      onPressed: null,
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.green,
-                      ),
-                      child: Text('Vrai',
-                      style: TextStyle(
-                        color:Colors.white,
-                        fontSize: 20
-                      ),
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
+              ),
+
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
                   child: TextButton(
-                    onPressed: null,
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.red,
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
                     ),
-                    child: Text('Faux',
-                        style: TextStyle(
-                          color:Colors.white,
-                          fontSize: 20
-                        ),
-                    ),
+                      onPressed: () {
+                      setState(() {
+                        suiviscore.add(Icon(Icons.check,color:Colors.green));
+
+                      });
+                      },
+                      child: Text("Vrai",
+                      style: TextStyle(color:Colors.white,
+                        fontSize: 25.0,
+                      ),
+                      ),
+
                   ),
                 ),
+              ),
 
-                Row(
-                  children:
-                   score
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        suiviscore.add(Icon(Icons.close,color:Colors.red));
 
+                      });
+                    },
+
+                    child: Text("Faux",
+                      style: TextStyle(color:Colors.white,
+                        fontSize: 25.0,
+                      ),
+                    ),
+
+                  ),
                 ),
-              ],
-            ),
+              ),
+              Expanded(
+                child: Row(
+                  children:suiviscore,
+                ),
+              )
+            ],
+
           ),
         ),
       ),
